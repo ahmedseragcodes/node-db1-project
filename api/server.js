e8eb65ea25f81e9538e73bff85964f0283f0f8bc
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-12 20:22:24 -0400

    wrote get, post, put routers and models

--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
+//IMPORTS
 const express = require("express");
-
+const accountsRouter = require("../api/accounts/accounts-router");
 const server = express();
 
+//GLOBAL MIDDLEWARE
 server.use(express.json());
+server.use("/api/accounts", accountsRouter );
+
+//CATCH ALL ENDPOINT
+server.get("/", (req, res)=>{
+    res.send(
+        "<h2>Arrivals</h2>"
+    )
+})
 
 module.exports = server;
